d6db5b9a42456b8bff9af6dcdc6f2a6211f43840
rwharton <rswharton95@gmail.com>
2025-08-11 15:45:27 +0200

    minor changes

--- gcpsr_params.py
+++ gcpsr_params.py
@@ -41,7 +41,7 @@ do_fold       = 1      # Fold data
 do_sp         = 1      # Single Pulse Search with TransientX
 
 # Filtool
-ftool_opts = '-t 24 -z kadaneF 4 8 zdot'
+ftool_opts = '-t 4 -z kadaneF 4 8 zdot'
 
 # Peasoup options 
 #peasoup -i data.fil \
@@ -55,7 +55,7 @@ ftool_opts = '-t 24 -z kadaneF 4 8 zdot'
 #        --dm_file my_dm_trials.txt \
 #        --ram_limit_gb 180.0 \
 #        -n 4
-psoup_opts = '-m 7.0 -t 1 --acc_start -100 --acc_end 100 --ram_limit_gb 20.0 --dm_start 500 --dm_end 3000 -n 4' 
+psoup_opts = '-m 7.0 -t 1 --acc_start -100 --acc_end 100 --ram_limit_gb 20.0 --dm_start 500 --dm_end 3000 -n 5' 
 
 # PulsarX folding
 # Using COMPACT peasoup candidate folding script 
